7261a6b2cf05c49f7ed2fe75fd54652c0f6180f3
Sven Vowe <svenvowe@gmail.com>
2024-12-02 12:38:56 +0100

    chore: added day 2

---
+++ src/2024-2.ts
@@ -0,0 +1,45 @@
+import { isEmpty } from 'lodash-es';
+import { reportsInput } from './source/2024-2-input';
+
+// preprocess reports
+const reports = reportsInput
+  .split('\n')
+  .map((line) => line.trim())
+  .filter((line) => !isEmpty(line))
+  .map((line) => line.split(' ').map(Number));
+
+// console.log('levels', reports);
+
+/**
+ * Get the differences between levels in a report
+ */
+const getDifferences = (report: number[]): number[] =>
+  report.flatMap((level, i, array) => (i === array.length - 1 ? [] : array[i + 1] - level));
+
+const differences = reports.map(getDifferences);
+
+/**
+ * a report is considered safe when all differences are between -3 and -1 or 1 and 3
+ */
+const getSafety = (differences: number[]): boolean => {
+  return (
+    differences.every((diff) => diff <= -1 && diff >= -3) ||
+    differences.every((diff) => diff >= 1 && diff <= 3)
+  );
+};
+
+// part 1: calculate safety of the differences directly
+const safety = differences.map(getSafety).filter(Boolean).length;
+console.log('safety', safety);
+
+// part 2: calculate safety with dampener
+const safetyWithDampener = reports
+  .map((report) =>
+    // whole array and the permutations when skipping  one level
+    [report, ...report.map((_, i, array) => [...array.slice(0, i), ...array.slice(i + 1)])]
+      .map(getDifferences)
+      .some(getSafety),
+  )
+  .filter(Boolean).length;
+
+console.log('safetyWithDampener', safetyWithDampener);
